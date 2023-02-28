c7dabce31ec9b02e5a4e6113802db74190b5d2b6
Nicholas M. Glykos <glykos@mbg.duth.gr>
2023-02-28 11:56:43 +0200

    First attempt to move to dimensions higher than 5.

--- words52_to_PCs.c
+++ words52_to_PCs.c
@@ -4,108 +4,162 @@
 #include <string.h>
 #include <omp.h>
 
-float   dat[25000000][5];
-int     indeces[25000000];
-int     nclu[25000000];
-int     N;
 
-char    currw[6];
-char    words[500000][6];
-int     c[500000];
+
+#define     MAX_STRUCT      25000000
+#define     MAX_PCs         10
+#define     MAX_CLU         500000
+
+
+float   dat[ MAX_STRUCT ][ MAX_PCs ];
+int     indeces[ MAX_STRUCT ];
+int     nclu[ MAX_STRUCT ];
+float   min[ MAX_PCs ];
+float   max[ MAX_PCs ];
+
+char    currw[ MAX_PCs+1 ];
+char    words[ MAX_CLU ][ MAX_PCs+1 ];
+int     cn[ MAX_CLU ];
 
 int     DIV;
 
+
 int main()
 {
-    int     i, k, m;
-    float   max1, min1, max2, min2, max3, min3, max4, min4, max5, min5;
+    int     i, k, m, N, c, off;
+    int     tot_col;
+    int     DIV;
     FILE    *clu;
     int     Nw = 0;
     char    in[1000];
+    char    *p;
+    float   val;
 
 
-    while( fgets( in, 1000, stdin ) != NULL )
-    {
-        if ( sscanf(in, "%d %f %f %f %f %f", &i, &dat[N][0], &dat[N][1], &dat[N][2], &dat[N][3], &dat[N][4] ) == 6 )
+    /* Find number of columns & read data */
+    if ( fgets( in, 1000, stdin ) == NULL )
         {
+                fprintf(stderr, "Failed to read any input (?!). Abort.\n");
+                exit( 1 );
+        }
+    p = &in[0];
+    tot_col = 0;
+    while ( sscanf( p, "%f%n", &val, &off) == 1 )
+            {
+                    tot_col++;
+                    p += off;
+            }
+    
+    N = 0;
+    do {
+        p = &in[0];
+
+        if ( sscanf( p, "%d%n", &i, &off ) == 1 )
             indeces[N] = i;
-            N++;
-        }
         else
-        {
-            fprintf(stderr, "Failed to read PCs at line %s\n", in );
-            exit( 1 );
-        }
+            {
+                fprintf(stderr, "Failed to read PCs at line %s\n", in );
+                exit( 1 );
+            }
+        
+        p += off;
+        for ( k=0 ; k < tot_col-1 ; k++ )
+            if ( sscanf( p, "%f%n", &val, &off ) == 1 )
+                {
+                    dat[N][k] = val;
+                    p += off;
+                }
+            else
+                {
+                    fprintf(stderr, "Failed to read PCs at line %s\n", in );
+                    exit( 1 );
+                }
+
+        N++;
+
+    } while ( fgets( in, 1000, stdin ) != NULL );
+
+    fprintf(stderr, "Read %d vectors (%d-dimensional).\n", N, tot_col-1 );
+
+
+    /* Determine limits & grid */
+    for ( i=0 ; i < tot_col-1 ; i++ )
+    {
+        min[i] = dat[0][i];
+        max[i] = dat[0][i];
     }
 
-
-    min1 = dat[0][0]; min2 = dat[0][1]; min3 = dat[0][2]; min4 = dat[0][3]; min5 = dat[0][4];
-    max1 = dat[0][0]; max2 = dat[0][1]; max3 = dat[0][2]; max4 = dat[0][3]; max5 = dat[0][4];
     for ( i=0 ; i < N ; i++ )
     {
-        if ( dat[i][0] < min1 ) min1 = dat[i][0];
-        if ( dat[i][0] > max1 ) max1 = dat[i][0];
-        if ( dat[i][1] < min2 ) min2 = dat[i][1];
-        if ( dat[i][1] > max2 ) max2 = dat[i][1];
-        if ( dat[i][2] < min3 ) min3 = dat[i][2];
-        if ( dat[i][2] > max3 ) max3 = dat[i][2];
-        if ( dat[i][3] < min4 ) min4 = dat[i][3];
-        if ( dat[i][3] > max4 ) max4 = dat[i][3];
-        if ( dat[i][4] < min5 ) min5 = dat[i][4];
-        if ( dat[i][4] > max5 ) max5 = dat[i][4];
+        for ( k=0 ; k < tot_col-1 ; k++ )
+        {
+            if ( dat[i][k] > max[k] )
+                max[k] = dat[i][k];
+
+            if ( dat[i][k] < min[k] )
+                min[k] = dat[i][k];
+        }
     }
 
-    DIV = (int)((2*pow( N, 0.20l))+0.50); 
+    /* fprintf(stderr, "%f %f %f %f\n", min[0], max[0], min[1], max[1] );   */
+    /* DIV = (int)((2*pow( N, (1.0l/(tot_col-1))))+0.50);                   */
+
+    DIV = (int)((2*pow( N, 0.20l))+0.50);
+
     if ( DIV > 51 )
         DIV = 51;
     if ( DIV < 5 )
         DIV = 5;
 
-    fprintf(stderr, "Read %d data points.\n", N );
-    fprintf(stderr, "Each dimension is divided into %d parts.\n", DIV );
+    fprintf(stderr, "Grid along each dimension set to %d.\n", DIV );
 
 
+
+    /* Read words and their corresponding cluster ID */
     clu = fopen( "cwords_clusters", "r" );
-    while( fscanf( clu, "%s %d", words[Nw], &c[Nw]) == 2 )
+    while( fscanf( clu, "%s %d", words[Nw], &cn[Nw]) == 2 )
         Nw++;
     fclose( clu );
 
     fprintf(stderr, "Read %d words.\n", Nw );
 
-    #pragma omp parallel for private( i, k, m, currw )
+
+    /* For every vector, find word and assign to cluster if present in the list */
+
+    #pragma omp parallel for private( i, k, m, c, currw )
     for ( i=0 ; i < N ; i++ )
     {
-        k = 'A'+(int)((dat[i][0]-min1)/((max1-min1)/DIV));
-        if ( k > 'Z' ) k += 6;
-        currw[0] = k;
 
-        k = 'A'+(int)((dat[i][1]-min2)/((max2-min2)/DIV));
-        if ( k > 'Z' ) k += 6;
-        currw[1] = k;
-        
-        k = 'A'+(int)((dat[i][2]-min3)/((max3-min3)/DIV));
-        if ( k > 'Z' ) k += 6;
-        currw[2] = k;
-        
-        k = 'A'+(int)((dat[i][3]-min4)/((max4-min4)/DIV));
-        if ( k > 'Z' ) k += 6;
-        currw[3] = k;
-        
-        k = 'A'+(int)((dat[i][4]-min5)/((max5-min5)/DIV));
-        if ( k > 'Z' ) k += 6;
-        currw[4] = k;
+        for ( k=0 ; k < tot_col-1 ; k++ )
+        {
+            c = 'A'+(int)((dat[i][k]-min[k])/((max[k]-min[k])/DIV));
+            if ( c > 'Z' ) 
+                c += 6;
+            currw[k] = c;
+        }
 
         for ( m=0 ; m < Nw ; m++ )
-            if ( currw[0] == words[m][0] && currw[1] == words[m][1] && currw[2] == words[m][2] && currw[3] == words[m][3] && currw[4] == words[m][4] )
             {
-                nclu[ i ] = c[m];
-                break;
+                for ( k=0 ; k < tot_col-1 ; k++ )
+                    if ( currw[k] != words[m][k] )
+                        break;
+
+                if ( k == tot_col-1 )
+                {
+                    nclu[ i ] = cn[m];
+                    break;
+                }
             }
     }
 
+    /* Output final list */
     for ( i=0 ; i < N ; i++ )
         if ( nclu[ i ] > 0 )
-            printf("%10d %4d  %10.7f %10.7f %10.7f %10.7f %10.7f\n", indeces[i], nclu[i], dat[i][0], dat[i][1], dat[i][2], dat[i][3], dat[i][4] );
-
+            {
+                printf("%10d %4d ", indeces[i], nclu[i] );
+                for ( k=0 ; k < tot_col-1 ; k++ )
+                    printf(" %10.7f", dat[i][k] );
+                printf("\n");
+            }
 }
 
--- words_52.c
+++ words_52.c
@@ -3,79 +3,117 @@
 #include <stdlib.h>
 #include <string.h>
 
-float   dat[25000000][5];
-int     indeces[25000000];
-int     N;
-int     DIV;
+
+#define     MAX_STRUCT      25000000
+#define     MAX_PCs         10
+
+
+float   dat[ MAX_STRUCT ][ MAX_PCs ];
+int     indeces[ MAX_STRUCT ];
+float   min[ MAX_PCs ];
+float   max[ MAX_PCs ];
+
 
 int main()
 {
-    int     i, k;
-    float   max1, min1, max2, min2, max3, min3, max4, min4, max5, min5;
+    int     i, k, c, off;
+    int     N;
+    int     DIV;
+    int     tot_col;
+    float   val;
     char    in[1000];
+    char    *p;
 
+
+    /* Find number of columns & read data */
+    if ( fgets( in, 1000, stdin ) == NULL )
+        {
+                fprintf(stderr, "Failed to read any input (?!). Abort.\n");
+                exit( 1 );
+        }
+    p = &in[0];
+    tot_col = 0;
+    while ( sscanf( p, "%f%n", &val, &off) == 1 )
+            {
+                    tot_col++;
+                    p += off;
+            }
+    
     N = 0;
-    while( fgets( in, 1000, stdin ) != NULL )
-    {
-        if ( sscanf(in, "%d %f %f %f %f %f", &i, &dat[N][0], &dat[N][1], &dat[N][2], &dat[N][3], &dat[N][4] ) == 6 )
-        {
+    do {
+        p = &in[0];
+
+        if ( sscanf( p, "%d%n", &i, &off ) == 1 )
             indeces[N] = i;
-            N++;
-        }
         else
-        {
-            fprintf(stderr, "Failed to read PCs at line %s\n", in );
-            exit( 1 );
-        }
+            {
+                fprintf(stderr, "Failed to read PCs at line %s\n", in );
+                exit( 1 );
+            }
+        
+        p += off;
+        for ( k=0 ; k < tot_col-1 ; k++ )
+            if ( sscanf( p, "%f%n", &val, &off ) == 1 )
+                {
+                    dat[N][k] = val;
+                    p += off;
+                }
+            else
+                {
+                    fprintf(stderr, "Failed to read PCs at line %s\n", in );
+                    exit( 1 );
+                }
+
+        N++;
+
+    } while ( fgets( in, 1000, stdin ) != NULL );
+
+    fprintf(stderr, "Read %d vectors (%d-dimensional).\n", N, tot_col-1 );
+
+
+    /* Determine limits & grid */
+    for ( i=0 ; i < tot_col-1 ; i++ )
+    {
+        min[i] = dat[0][i];
+        max[i] = dat[0][i];
     }
 
-    min1 = dat[0][0]; min2 = dat[0][1]; min3 = dat[0][2]; min4 = dat[0][3]; min5 = dat[0][4];
-    max1 = dat[0][0]; max2 = dat[0][1]; max3 = dat[0][2]; max4 = dat[0][3]; max5 = dat[0][4];
     for ( i=0 ; i < N ; i++ )
     {
-        if ( dat[i][0] < min1 ) min1 = dat[i][0];
-        if ( dat[i][0] > max1 ) max1 = dat[i][0];
-        if ( dat[i][1] < min2 ) min2 = dat[i][1];
-        if ( dat[i][1] > max2 ) max2 = dat[i][1];
-        if ( dat[i][2] < min3 ) min3 = dat[i][2];
-        if ( dat[i][2] > max3 ) max3 = dat[i][2];
-        if ( dat[i][3] < min4 ) min4 = dat[i][3];
-        if ( dat[i][3] > max4 ) max4 = dat[i][3];
-        if ( dat[i][4] < min5 ) min5 = dat[i][4];
-        if ( dat[i][4] > max5 ) max5 = dat[i][4];
+        for ( k=0 ; k < tot_col-1 ; k++ )
+        {
+            if ( dat[i][k] > max[k] )
+                max[k] = dat[i][k];
+
+            if ( dat[i][k] < min[k] )
+                min[k] = dat[i][k];
+        }
     }
 
-    DIV = (int)((2*pow( N, 0.20l))+0.50); 
+    /* fprintf(stderr, "%f %f %f %f\n", min[0], max[0], min[1], max[1] );   */
+    /* DIV = (int)((2*pow( N, (1.0l/(tot_col-1))))+0.50);                   */
+
+    DIV = (int)((2*pow( N, 0.20l))+0.50);
+
     if ( DIV > 51 )
         DIV = 51;
     if ( DIV < 5 )
         DIV = 5;
 
-    fprintf(stderr, "Read %d data points.\n", N );
-    fprintf(stderr, "Will divide each dimension in %d parts.\n", DIV );
+    fprintf(stderr, "Grid along each dimension set to %d.\n", DIV );
 
+
+    /* Prepare word list */
     for ( i=0 ; i < N ; i++ )
     {
-        k = 'A'+(int)((dat[i][0]-min1)/((max1-min1)/DIV));
-        if ( k > 'Z' ) k += 6;
-        printf("%c", (char)(k) );
-
-        k = 'A'+(int)((dat[i][1]-min2)/((max2-min2)/DIV));
-        if ( k > 'Z' ) k += 6;
-        printf("%c", (char)(k) );
-        
-        k = 'A'+(int)((dat[i][2]-min3)/((max3-min3)/DIV));
-        if ( k > 'Z' ) k += 6;
-        printf("%c", (char)(k) );
-        
-        k = 'A'+(int)((dat[i][3]-min4)/((max4-min4)/DIV));
-        if ( k > 'Z' ) k += 6;
-        printf("%c", (char)(k) );
-        
-        k = 'A'+(int)((dat[i][4]-min5)/((max5-min5)/DIV));
-        if ( k > 'Z' ) k += 6;
-        printf("%c\n", (char)(k) );
-
+        for ( k=0 ; k < tot_col-1 ; k++ )
+        {
+            c = 'A'+(int)((dat[i][k]-min[k])/((max[k]-min[k])/DIV));
+            if ( c > 'Z' ) 
+                c += 6;
+            printf("%c", (char)(c) );
+        }
+        printf("\n");
     }
 
 }
