5a60c5c5d16a6a0642338bb629a82583ab0a8fb2
Nicholas M. Glykos <glykos@mbg.duth.gr>
2023-04-10 23:26:46 +0300

    Still fighting with cutoff with low/high dimensionality, and few data points. This is the current situation.

--- merge.c
+++ merge.c
@@ -20,7 +20,7 @@ int main()
     int     added, current=0;
     int     i, k, m, dist, printed;
     int     nofPCs = -1; 
-
+    int     cut;
 
     /* Read list of words */
     while( scanf("%s", wori[N] ) == 1 )
@@ -57,7 +57,11 @@ int main()
 
     added = 1;
     printed = 0;
-    
+     
+    cut = nofPCs / 2;
+    if ( cut < 1 )
+        cut = 1;
+
     while( added > 0 )
     {
         added = 0;
@@ -71,14 +75,16 @@ int main()
                 {
                     if ( c[k] == 0 )
                     {
-                        dist = abs(w[i][0]-w[k][0]) + abs(w[i][1]-w[k][1]) + abs(w[i][2]-w[k][2]) + abs(w[i][3]-w[k][3]) + abs(w[i][4]-w[k][4]) +
-                               abs(w[i][5]-w[k][5]) + abs(w[i][6]-w[k][6]) + abs(w[i][7]-w[k][7]) + abs(w[i][8]-w[k][8]) + abs(w[i][9]-w[k][9]) ;
+                        dist = 4*abs(w[i][0]-w[k][0]) + 3*abs(w[i][1]-w[k][1]) + 2*abs(w[i][2]-w[k][2]) + abs(w[i][3]-w[k][3]) + abs(w[i][4]-w[k][4]) +
+                                 abs(w[i][5]-w[k][5]) +   abs(w[i][6]-w[k][6]) +   abs(w[i][7]-w[k][7]) + abs(w[i][8]-w[k][8]) + abs(w[i][9]-w[k][9]) ;
 
                          /* dist += abs(w[i][m]-w[k][m]) ; */
                          /* dist += (nofPCs-m)*abs(w[i][m]-w[k][m]) ; */
                          /* dist += (nofPCs-m-1>0?nofPCs-m-1:1)*abs(w[i][m]-w[k][m]) ; */
-
-                        if ( dist <= nofPCs )
+                         
+                         /* if ( dist <= cut ) */ 
+                        
+                        if ( dist < 10 )
                             {
                                 c[k] = current;
                                 added++;
